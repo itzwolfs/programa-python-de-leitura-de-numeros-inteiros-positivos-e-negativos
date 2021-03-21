7fd7a879b9befc7cf18eb1f4b63329057e6820ab
itzwolfs <80694017+itzwolfs@users.noreply.github.com>
2021-03-20 21:24:37 -0300

    Update 5.py

--- 5.py
+++ 5.py
@@ -1,5 +1,11 @@
-n1 = int(input('Digite o primero numero: '))
+#primeiro iremos colocar o n1, para ser a nossa base de indentifica
+#logo em seguida vamos usar o int(input ('dentro colocaremos o que irá aparecer na tela do python
+#para encerra iremos colocar o if apontando o nosso numero com o simbolo menor que 1
+#abaixo da variante if iremos colocar o print ('voce colocar o que irá aparecer no logo sendo print if menor que zero e else maior que zero')
+n1 = int(input('Digite o primero numero: ')
 if n1 < 0:
-    print('menor que 0 é negativo')
+    print('add')
 else:
-    print('maior que zero é positivo')
+    print('add')
+#isso é para te dar uma base e apoio porém voce pode deixar ainda melhor com loops adicionando o novo print embaixo do else
+#espero ter ajudado em sua base de de exercicio ou aprendizado basico em py
